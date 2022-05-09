311087664b30d75ea90f7904ec94f68a5bcfc45a
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:39 -0300

    chore: re index bitvec

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bitvec-master".dir   = "master";
-  inputs."bitvec-master".owner = "nim-nix-pkgs";
-  inputs."bitvec-master".ref   = "master";
-  inputs."bitvec-master".repo  = "bitvec";
-  inputs."bitvec-master".type  = "github";
-  inputs."bitvec-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bitvec-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
